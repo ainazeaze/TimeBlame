cfa7b09cd7fac1e269763a3975ca25c64dc96033
ainazeaze <aratefiarivony8@gmail.com>
2026-02-20 21:53:24 +0100

    build : some project initation blahblah

---
+++ time-blame/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from time-blame!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ time-blame/time_blame/__init__.py
@@ -0,0 +1,3 @@
+"""Git Time Machine - Browse file history with a TUI."""
+
+__version__ = "0.1.0"
